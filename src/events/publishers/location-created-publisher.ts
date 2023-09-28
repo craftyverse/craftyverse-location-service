cd8245353c802e38565275fe13e8433eec07a972
Tony Li <tonyatlcs@gmail.com>
2023-09-28 15:51:21 +1000

    Deleted NATS implementation

--- src/events/publishers/location-created-publisher.ts
+++
@@ -1,9 +0,0 @@
-import {
-  Publisher,
-  Subjects,
-  LocationCreatedEvent,
-} from "@craftyverse-au/craftyverse-common";
-
-export class LocationCreatedPublisher extends Publisher<LocationCreatedEvent> {
-  subject: Subjects.LocationCreated = Subjects.LocationCreated;
-}
